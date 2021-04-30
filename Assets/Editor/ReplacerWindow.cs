0aa456dd0ffccfa74f9f398d1b4d2b2ee8965572
BottaDev <fedebotta@gmail.com>
2021-04-29 22:27:58 -0300

    Added x-y-z behaviour

--- Assets/Editor/ReplacerWindow.cs
+++ Assets/Editor/ReplacerWindow.cs
@@ -6,8 +6,18 @@ using UnityEditor;
 public class ReplacerWindow : EditorWindow
 {
     private GameObject _objectReplace;
-    private bool _useRotation = true;
-    private bool _useScale = true;
+    
+    // Rotation
+    private bool _useRotation = false;
+    private bool _useRotX = true;
+    private bool _useRotZ = true;
+    private bool _useRotY = true;
+    
+    // Scale
+    private bool _useScale = false;
+    private bool _useScaX = true;
+    private bool _useScaZ = true;
+    private bool _useScaY = true;
 
     [MenuItem("CustomTools/Replacer")]
     public static void OpenWindow()
@@ -15,7 +25,7 @@ public class ReplacerWindow : EditorWindow
         ReplacerWindow window = GetWindow<ReplacerWindow>();
 
         window.wantsMouseMove = true;
-        window.minSize = new Vector2(300, 110);
+        window.minSize = new Vector2(300, 230);
     }
     
     private void OnGUI()
@@ -23,7 +33,24 @@ public class ReplacerWindow : EditorWindow
         _objectReplace = EditorGUILayout.ObjectField("Object Replace: ", _objectReplace, typeof(GameObject), true) as GameObject;
 
         _useRotation = EditorGUILayout.Toggle("Use Rotation", _useRotation);
+        if (_useRotation)
+        {
+            GUILayout.BeginVertical ("box");
+            _useRotX = EditorGUILayout.Toggle("Use X", _useRotX);
+            _useRotY = EditorGUILayout.Toggle("Use Y", _useRotY);
+            _useRotZ = EditorGUILayout.Toggle("Use Z", _useRotZ);
+            GUILayout.EndVertical();
+        }
+
         _useScale = EditorGUILayout.Toggle("Use Scale", _useScale);
+        if (_useScale)
+        {
+            GUILayout.BeginVertical ("box");
+            _useScaX = EditorGUILayout.Toggle("Use X", _useScaX);
+            _useScaY = EditorGUILayout.Toggle("Use Y", _useScaY);
+            _useScaZ = EditorGUILayout.Toggle("Use Z", _useScaZ);
+            GUILayout.EndVertical();
+        }
         
         if (GUILayout.Button("Replace selected objects", GUILayout.Height(50)) && _objectReplace != null)
             ReplaceObjects();
@@ -41,8 +68,22 @@ public class ReplacerWindow : EditorWindow
         {
             GameObject newObject = Instantiate(_objectReplace, obj.transform.position, _objectReplace.transform.rotation);
 
-            newObject.transform.rotation = _useRotation ? obj.transform.rotation : newObject.transform.rotation;
-            newObject.transform.localScale = _useScale ? obj.transform.localScale : newObject.transform.localScale;
+            if (_useRotation)
+            {
+                newObject.transform.rotation = new Quaternion(
+                    _useRotX ? obj.transform.rotation.x : newObject.transform.rotation.x,
+                    _useRotY ? obj.transform.rotation.y : newObject.transform.rotation.y,
+                    _useRotZ ? obj.transform.rotation.z : newObject.transform.rotation.z,
+                    obj.transform.rotation.w);
+            }
+
+            if (_useScale)
+            {
+                newObject.transform.localScale = new Vector3(
+                    _useScaX ? obj.transform.localScale.x : newObject.transform.localScale.x,
+                    _useScaY ? obj.transform.localScale.y : newObject.transform.localScale.y,
+                    _useScaZ ? obj.transform.localScale.z : newObject.transform.localScale.z);
+            }
 
             DestroyImmediate(obj);
         }
